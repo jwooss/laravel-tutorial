23b206a3571914e8b1f477eb2bd6808bb764cb9c
KimJaeWoos <wodn4131@gmail.com>
2018-10-04 00:30:23 +0900

    뷰와 데이터 바인딩

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', [
-    'as' => 'home',
-    function () {
-        return '제 이름은 home 입니다';
-    }
-]);
-Route::get('/home', function () {
-    return redirect(route('home'));
+Route::get('/', function () {
+    return view('welcome', [
+        'name' => 'Foo',
+        'greeting' => '안녕하세요?',
+    ]);
 });
