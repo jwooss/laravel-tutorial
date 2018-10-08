1e2d633704136cecb666775f2cf9b6d223abafb5
KimJaeWoos <wodn4131@gmail.com>
2018-10-09 02:29:03 +0900

    데이터베이스 시딩

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (config('database.default') !== 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        }
+
+        // delete() 는 단순히 데이터를 지우지만
+        // truncate() 는 데이터를 지우고 기본키를 1부터 재배열 한다
+        App\User::truncate();
+        $this->call(UsersTableSeeder::class);
+
+        App\Article::truncate();
+        $this->call(ArticlesTableSeeder::class);
+
+        if (config('database.default') !== 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
     }
 }
